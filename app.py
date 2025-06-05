d203ab0960b50e908bd97d024ccba0e9cfa95367
Torinji <Torinji.san@gmail.com>
2025-06-05 12:55:03 +0200

    Add run with specific port

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, Response
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
-
-@app.route("/health")
+@app.route('/health')
 def health():
-    return Response("OK", status=200)
+    return "OK", 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    port = int(os.environ.get('APP_PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
